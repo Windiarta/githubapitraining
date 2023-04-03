3e1c54a53fa3019248b62102c56cceb3366e9047
Windiarta <windiarta@ui.ac.id>
2023-04-03 18:51:19 +0700

    done

--- app/src/main/java/com/example/githubapi/ApiService.kt
+++ app/src/main/java/com/example/githubapi/ApiService.kt
@@ -5,29 +5,29 @@ import retrofit2.http.*
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    //@Headers("Authorization: token [your token]")
     fun userSearch(
         @Query("q") username: String
     ): Call<SearchResponse>
 
     @GET("users")
-    @Headers("Authorization: token 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")
+    //@Headers("Authorization: token [your token]")
     fun usersSearch(): Call<List<ItemsItem>>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    //@Headers("Authorization: token [your token]")
     fun userDetail(
         @Path("username") username: String
     ): Call<DetailResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    //@Headers("Authorization: token [your token]")
     fun userFollower(
         @Path("username") username: String
     ): Call<List<ItemsItem>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    //@Headers("Authorization: token [your token]")
     fun userFollowing(
         @Path("username") username: String
     ): Call<List<ItemsItem>>
